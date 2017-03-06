c9b7a7cf96e23311620870ef3e58abac226ceab8
AlanJager <873863981@qq.com>
2017-03-06 23:22:41 +0800

    add test

--- src/test/mockito/test/TestAsyncMsgFlow.java
+++ src/test/mockito/test/TestAsyncMsgFlow.java
@@ -2,7 +2,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import test.mockito.src.*;
 
