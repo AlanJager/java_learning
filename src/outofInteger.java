37ef4c4142035ba34a671583ab7cf2b3d2ff561f
AlanJager <873863981@qq.com>
2017-03-11 22:15:13 +0800

    Test out of Integer

---
+++ src/outofInteger.java
@@ -0,0 +1,12 @@
+/**
+ * Created by AlanJager on 2017/3/8.
+ */
+public class outofInteger {
+    public static void main(String[] args) {
+        int t = 106400;
+        int c = 1000;
+        int times = 1000;
+
+        System.out.println((long)t * c * times);
+    }
+}
